fa9185414d7517b3c9e0d047ccd00ce937e7757c
arianahabas <arianahabas@gmail.com>
2020-07-17 10:52:09 -0500

    added basic styles

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,6 +3,20 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import styled from 'styled-components'
 
+const StyledContainer = styled.div`
+    display:flex;
+    flex-wrap:wrap;
+    justify-content: space-around;
+ `
+const StyledCard = styled.div`
+    display:flex;
+    flex-direction:column;
+    border: 3px solid blue;
+    padding: 15px;
+    margin: 15px;
+    width: 15vw;
+    background-color:white;
+`
 
 
 
@@ -25,18 +39,24 @@ export default function Character (){
 
 
     const CharacterCard = ({info}) => (
-            <div>
-                <h2>{info.name}</h2>
-                <img src={info.image} alt=''></img>
-                <p> Gender: {info.gender}</p>
-                <p> Species: {info.species}</p>
-                <p> Status: {info.status}</p>
-            </div>
+            
+                <StyledCard>
+                    <h2>{info.name}</h2>
+                    <img src={info.image} alt=''></img>
+                    <p> Gender: {info.gender}</p>
+                    <p> Species: {info.species}</p>
+                    <p> Status: {info.status}</p>
+                </StyledCard>
+     
     )
     return (
-        card.map((p) => {
-            return <CharacterCard key ={p.id} info={p} />
-        })
+        <StyledContainer>
+            {
+            card.map((p) => {
+                return <CharacterCard key ={p.id} info={p} />
+            })
+            }
+        </StyledContainer>
     )
 
 }
